09faa613fbcf78d11d9cc3df725309bf0b8a5288
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2016-05-16 17:57:19 -0300

    AAAAH

--- src/Apriori.java
+++ src/Apriori.java
@@ -1,28 +1,18 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
-import java.util.StringTokenizer;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
-import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.commons.io.FileUtils;
 
 public class Apriori {
 	
-	public static int SUPORTE_MINIMO = 5; // porcentagem
+	public static int SUPORTE_MINIMO = 15; // porcentagem
 	public static int TRANSACOES = 0;
 	
 	public static HashMap<String, Set<String>> contagem = new HashMap<String, Set<String>>();
--- src/MapContagem.java
+++ src/MapContagem.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -12,16 +15,67 @@ public class MapContagem extends Mapper<LongWritable, Text, Text, Text> {
         @Override
         public void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
-//        	String line = value.toString();
-//            String[] values = line.trim().split(" ");
-//            Apriori.TRANSACOES += 1;
-//            
-//            for (int i = 0; i < values.length; i++) {
-//            	output.collect(new Text(values[i]), new Text(Apriori.TRANSACOES + ""));
-//            }
+        	String val = value.toString();
+            String[] linhas = val.trim().split("\n");
+            
+            List<Set<String>> itemsets = apriori(linhas);
+        }
+        
+        public List<Set<String>> apriori(String[] linhas)
+        {
+        	ArrayList<ArrayList<String>> transacoes = new ArrayList<ArrayList<String>>();
+        	HashMap<String, List<String>> ocorrencias = new HashMap<String, List<String>>();
         	
-        	System.out.println(key.toString());
-        	System.out.println(value.toString());
-        	System.out.println("-----------");
+        	for (int i = 0; i < linhas.length; i++) {
+        		String[] itens = linhas[i].trim().split(" ");
+        		
+        		transacoes.add(new ArrayList<String>(Arrays.asList(itens)));
+        		
+        		for (int j = 0; j < itens.length; j++) {
+        			if (ocorrencias.get(itens[j]) == null) {
+        				ocorrencias.put(itens[j], new ArrayList<String>());
+        			}
+        			
+        			ocorrencias.get(itens[j]).add(Integer.toString(i));
+        		}
+        	}
+        	
+        	Set<String> keys = ocorrencias.keySet();
+        	List<String> toRemove = new ArrayList<String>();
+        	
+        	for (Iterator<String> iterator = keys.iterator(); iterator.hasNext();) {
+        		String k = iterator.next();
+        		int count = ocorrencias.get(k).size();
+        		
+        		double suporte = (100.0f * count) / linhas.length;
+        		
+        		if (suporte <= Apriori.SUPORTE_MINIMO) {
+        			toRemove.add(k);
+        		}
+        	}
+        	
+        	for (int i = 0; i < toRemove.size(); i++) {
+        		ocorrencias.remove(toRemove.get(i));
+        	}
+        	
+        	keys = ocorrencias.keySet();
+        	
+        	for (Iterator<String> iterator = keys.iterator(); iterator.hasNext();) {
+        		String k = iterator.next();
+        		System.out.println(k + " : " + ocorrencias.get(k));
+        	}
+        	
+        	int k = 0;
+        	boolean continuar = true;
+        	
+        	while (continuar) {
+        		continuar = false;
+        		
+        		
+        		
+        		k++;
+        	}
+        	
+        	return new ArrayList<Set<String>>();
         }
     }
